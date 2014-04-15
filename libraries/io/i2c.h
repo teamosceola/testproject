11971a473ddefc74e861ff68084b54f56139050c
Chris Crews <christopher.crews07@gmail.com>
2014-04-14 22:05:23 -0500

    Added basics of i2c header

--- libraries/io/i2c.h
+++ libraries/io/i2c.h
@@ -1,6 +1,22 @@
 #ifndef I2C_H
 #define I2C_H
 
+ class i2c
+ {
+ private:
+ 	static int fd;
 
+ 	i2c() {}
+ 	~i2c();
+ 	int open(int nValue);
+ 	int close();
+
+public:
+ 	i2c(int nValue);
+ 	int write();
+ 	int write_register();
+ 	int read();
+ 	int read_register();
+ };
 
 #endif
